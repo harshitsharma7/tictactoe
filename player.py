c8b53bc85e5aa00d06e5cd104b0d51349c6aba8f
Harshit Sharma <harshitsharma@Harshits-MacBook-Air.local>
2022-06-03 01:08:15 +0530

    Game built - Dumb Computer

--- player.py
+++ player.py
@@ -14,11 +14,24 @@ class RandomComputerPlayer(Player):
         super().__init__(letter)
 
     def get_move(self, game):
-        return super().get_move(game)
+        return random.choice(game.available_moves())
 
 class HumanPlayer(Player):
     def __init__(self, letter):
         super().__init__(letter)
 
     def get_move(self, game):
-        return super().get_move(game)
+        valid_move = False
+        val = None
+        while not valid_move:
+            square = input(self.letter + '\'s turn. Input move 0:9')
+
+            try:
+                val = int(square)
+                if val not in game.available_moves():
+                    raise ValueError
+                valid_move = True
+            except ValueError:
+                print("Please retry with a valid move.")
+
+        return val
